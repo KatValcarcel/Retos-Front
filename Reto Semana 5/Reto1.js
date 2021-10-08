c5ffa063acc99f0ef70104a2ca6933e92173a14c
Kat Valcarcel <katherine_valcarcel1@usmp.pe>
2021-10-08 16:15:53 -0500

    Reto 5 terminado

--- Reto Semana 5/Reto1.js
+++ Reto Semana 5/Reto1.js
@@ -5,3 +5,19 @@ const selectElement = (element) => document.querySelector(element);
 selectElement('.menu-icons').addEventListener('click', () => {
     selectElement('nav').classList.toggle('active');
 });
+
+document.getElementById('litros').oninput = calcular;
+document.getElementById('pGalon').oninput = calcular;
+
+function calcular(){
+    let L = document.getElementById('litros').value;
+    if (!L) return;
+    let PG = document.getElementById('pGalon').value;
+    if (!PG) return;
+    // conversion a galones
+    let TG = (L/3.785).toFixed(2);
+    // ganancia
+    let GA = (PG * TG).toFixed(2);   
+    document.getElementById('galon').value = TG;
+    document.getElementById('ganancia').value = GA;
+}
